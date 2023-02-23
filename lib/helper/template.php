7f1d3cb7189e16d0316b8ee69cc1eb52cd67455c
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-02-23 15:16:45 +0700

    Update template.php

--- lib/helper/template.php
+++ lib/helper/template.php
@@ -1,10 +1,10 @@
 <?php
-    /**
+/**
  * Author   : Muhammad Deril
  * URI      : http://www.derillab.com
  * Github   : @derilkillms
  */
-    
+
     require_once (__DIR__ .'/Mustache/Autoloader.php');
 
     Mustache_Autoloader::register();
@@ -13,10 +13,6 @@
     $getactive = $DB->get_record_sql("SELECT * FROM {themes} WHERE status=1");
 
     $mustache = new Mustache_Engine(array(
-       'template_class_prefix' => '__MyTemplates_',
-       'cache' => dirname(__FILE__).'/tmp/cache/mustache',
-    'cache_file_mode' => 0666, // Please, configure your umask instead of doing this :)
-    'cache_lambda_templates' => true,
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../../d-content/themes/'.$getactive->path),
     'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../../d-content/themes/'.$getactive->path.'/partials'),
     'helpers' => array('i18n' => function($text) {
